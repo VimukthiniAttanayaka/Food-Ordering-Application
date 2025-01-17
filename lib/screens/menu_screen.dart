b5e6aeb90edb695064be727a43e11fa34a486781
A.M.V.N.Attanayaka <73266263+VimukthiniAttanayaka@users.noreply.github.com>
2025-01-17 20:48:09 +0530

    read menu list data from file

--- lib/screens/menu_screen.dart
+++ lib/screens/menu_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:food_ordering_application/providers/menu_provider.dart';
+import 'package:provider/provider.dart';
 
 class MenuScreen extends StatefulWidget {
   const MenuScreen({Key? key}) : super(key: key);
@@ -8,10 +10,26 @@ class MenuScreen extends StatefulWidget {
 }
 
 class _MenuScreenState extends State<MenuScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    context.read<MenuProvider>().fetchMenuData();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final menuProvider = Provider.of<MenuProvider>(context);
     return Scaffold(
-      body: Text('fghj'),
+      body: ListView.builder(
+        itemCount: menuProvider.menus.length,
+        itemBuilder: (context, index) {
+          final menu = menuProvider.menus[index];
+          return ListTile(
+            title: Text(menu.title['en'] ?? ''),
+          );
+        },
+      ),
     );
   }
 }
